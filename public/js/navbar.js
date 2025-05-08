bc766013706177047db9a1f081c377560f13e18f
Emma Clark <emmabossuytclark@gmail.com>
2025-05-08 11:16:33 -0300

    added navbar html and fetch to import navbar into ejs files

---
+++ public/js/navbar.js
@@ -0,0 +1,20 @@
+fetch("/html/navbar.html") // Adjust the path to reflect the correct location
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById("nav-placeholder").innerHTML = data;
+
+    const menuIcon = document.getElementById("menu_icon");
+    const navigationContainer = document.getElementById("navigation_container");
+    menuIcon.addEventListener("click", () => {
+      menuIcon.classList.toggle("show_menu");
+      navigationContainer.classList.toggle("show_nav");
+    });
+
+    navigationContainer.addEventListener("mouseleave", () => {
+      menuIcon.classList.toggle("show_menu");
+      navigationContainer.classList.toggle("show_nav");
+    });
+  })
+  .catch((error) => {
+    console.error("Error loading the nav:", error);
+  });
